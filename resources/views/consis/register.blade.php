8fa117a19c0c496247ac4897d2720944d863bc6b
vanfranrocha <vanfranrocha@hotmail.com>
2016-06-23 18:18:24 -0300

    Seção Inscricoes Congresso

--- resources/views/consis/register.blade.php
+++ resources/views/consis/register.blade.php
@@ -2,22 +2,10 @@
     <section class="row ticket">
         <div class="container">
             <div class="row booking_ticket">
-                <h2><span>Book</span> Your <span>Tickets</span> Now to Confirm Your Seat!</h2>
+                <h2><span>Inscrições </span> para o I Congresso de Sistemas de Informações <h4 style="color: #fff"; >Não deixe de participar do I Congresso de Sistemas de Informação FAHESA/ITPAC.<br> Venha trocar experiências em um ambiente agradável e inclusivo.<h4/></h2>
                 <div class="register_form">
                     <form class="form-inline">
-                        <div class="form-group">
-                            <label for="InputName"><i class="fa fa-user"></i></label>
-                            <input type="text" class="form-control" id="InputName" placeholder="full name">
-                        </div>
-                        <div class="form-group">
-                            <label for="InputEmail"><i class="fa fa-envelope"></i></label>
-                            <input type="email" class="form-control" id="InputEmail" placeholder="email id">
-                        </div>
-                        <div class="form-group">
-                            <label for="Inputphone"><i class="fa fa-phone"></i></label>
-                            <input type="text" class="form-control" id="Inputphone" placeholder="phone no">
-                        </div>
-                        <a href="#">register now</a>
+                        <a href="#">Inscrições em Breve</a>
                     </form>
                 </div>
             </div>
