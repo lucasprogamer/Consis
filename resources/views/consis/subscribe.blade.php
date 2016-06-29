dcaee94cdb15a453e06bc42930af32713275b09c
vanfranrocha <vanfranrocha@hotmail.com>
2016-06-28 21:29:47 -0300

    submissoes correcao

--- resources/views/consis/subscribe.blade.php
+++ resources/views/consis/subscribe.blade.php
@@ -1,8 +1,8 @@
     <section class="row newsletter">
         <div class="container">
             <div class="row content">
-                <h2>Chamada<span> Submissões </span>
-                <h2 style="font-size: 16px;padding-bottom: 0px;font-weight: normal;">Submeta seus artigos no congresso</h2>
+                <h2>Chamada<span> Submissões <h2 style="font-size: 16px;padding-bottom: 0px;font-weight: normal;">Submeta seus artigos no congresso</h2></span>
+                
                 </h2>
                 <form class="form-inline">
                     <div class="input-group">
