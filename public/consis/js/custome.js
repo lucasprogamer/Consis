d5c67fcaf32db37030638fbb8400f15b65e716e7
lucasprogamer <lucasouzaweb@hotmail.com>
2016-06-29 11:24:48 -0300

    test maps

--- public/consis/js/custome.js
+++ public/consis/js/custome.js
@@ -94,7 +94,32 @@
     // Lightbox
      lightbox.option({
         'showImageNumberLabel': false,  
-    })
+    });
+
+     // Maps
+    var map;
+        $(document).ready(function(){
+            map = new GMaps({
+                el: '#map',
+                lat: -7.208513,
+                lng: -48.243873,
+                zoomControl : false, 
+                zoomControlOpt: {
+                style : 'SMALL',
+                position: 'TOP_LEFT'
+            },
+            panControl : false,
+            streetViewControl : false, 
+            mapTypeControl: false,
+            scrollwheel: false,
+            overviewMapControl: false
+        });
+        map.addMarker({
+            lat: -7.2092157,
+            lng: -48.2370496,
+            title: 'I Consis', 
+        });
+    });
     
     
  
