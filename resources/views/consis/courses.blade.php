3034dc7d67c94216e508cda840c7233a231c59c0
vanfranrocha <vanfranrocha@hotmail.com>
2016-06-29 14:36:52 -0300

    Conteudo minicursos

--- resources/views/consis/courses.blade.php
+++ resources/views/consis/courses.blade.php
@@ -12,8 +12,8 @@
             <a href="consis/images/minicurso/minicursowordpress.jpg" data-lightbox="roadtrip">
                 <img src="consis/images/minicurso/minicursowordpress.jpg" alt="">
                 <div class="item_back">
-                    <h4>Aqui vai o titulo</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivam at usaliquet orci. Ut interdum mauris sem, non aliquet felis inter. dum sit</p>
+                    <h4>Desmistificando o Wordpress no Desenvolvimento de Grandes Sites</h4>
+                    <p>Palestrante: Bruno Fernando e Lucas Alves</p>
                 </div>
             </a> 
         </div>
@@ -21,8 +21,8 @@
             <a href="consis/images/minicurso/minicursoarduino.jpg" data-lightbox="roadtrip">
                 <img src="consis/images/minicurso/minicursoarduino.jpg" alt="">
                 <div class="item_back">
-                    <h4>Next Web Design  Event at Sunday</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivam at usaliquet orci. Ut interdum mauris sem, non aliquet felis inter. dum sit</p>
+                    <h4>Arduíno</h4>
+                    <p>Palestrante: Carlos Andrew e Kaic Valadares</p>
                 </div>
             </a> 
         </div>
@@ -30,8 +30,8 @@
             <a href="consis/images/minicurso/minicursointelxdk.jpg" data-lightbox="roadtrip">
                 <img src="consis/images/minicurso/minicursointelxdk.jpg" alt="">
                 <div class="item_back">
-                    <h4>Next Web Design  Event at Sunday</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivam at usaliquet orci. Ut interdum mauris sem, non aliquet felis inter. dum sit</p>
+                    <h4>Desenvolvimento de Aplicativos com Intel XDK</h4>
+                    <p>Palestrante: André Rincon</p>
                 </div>
             </a> 
         </div>
@@ -39,8 +39,8 @@
             <a href="consis/images/minicurso/minicursocoding.jpg" data-lightbox="roadtrip">
                 <img src="consis/images/minicurso/minicursocoding.jpg" alt="">
                 <div class="item_back">
-                    <h4>Next Web Design  Event at Sunday</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivam at usaliquet orci. Ut interdum mauris sem, non aliquet felis inter. dum sit</p>
+                    <h4>Primeiros Passos para Validar sua Idéia e Montar uma Startup</h4>
+                    <p>Palestrante: Jefferson (Sebrae)</p>
                 </div>
             </a>
         </div>
@@ -51,8 +51,8 @@
             <a href="consis/images/minicurso/minicursostartup.jpg" data-lightbox="roadtrip">
                 <img src="consis/images/minicurso/minicursostartup.jpg" alt="">
                 <div class="item_back">
-                    <h4>Next Web Design  Event at Sunday</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivam at usaliquet orci. Ut interdum mauris sem, non aliquet felis inter. dum sit</p>
+                    <h4>Coding Dojo</h4>
+                    <p>Palestrante: Edivan Ricardo</p>
                 </div>
             </a> 
         </div>
@@ -60,8 +60,8 @@
             <a href="consis/images/minicurso/scrum.jpg" data-lightbox="roadtrip">
                 <img src="consis/images/minicurso/minicursoscrum.jpg" alt="">
                 <div class="item_back">
-                    <h4>Next Web Design  Event at Sunday</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivam at usaliquet orci. Ut interdum mauris sem, non aliquet felis inter. dum sit</p>
+                    <h4>SCRUM na Prática</h4>
+                    <p>Palestrante: Marcia Savoine</p>
                 </div>
             </a> 
         </div>
@@ -69,8 +69,8 @@
             <a href="http://placehold.it/200x200" data-lightbox="roadtrip">
                 <img src="http://placehold.it/200x200" alt="">
                 <div class="item_back">
-                    <h4>Next Web Design  Event at Sunday</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivam at usaliquet orci. Ut interdum mauris sem, non aliquet felis inter. dum sit</p>
+                    <h4 style="text-align: center;padding-top: 60px;">Em Breve</h4>
+                    <p><!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivam at usaliquet orci. Ut interdum mauris sem, non aliquet felis inter. dum sit --></p>
                 </div>
             </a> 
         </div>
@@ -78,8 +78,8 @@
             <a href="http://placehold.it/200x200" data-lightbox="roadtrip">
                 <img src="http://placehold.it/200x200" alt="">
                 <div class="item_back">
-                    <h4>Next Web Design  Event at Sunday</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivam at usaliquet orci. Ut interdum mauris sem, non aliquet felis inter. dum sit</p>
+                    <h4 style="text-align: center;padding-top: 60px;">Em Breve</h4>
+                    <p><!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivam at usaliquet orci. Ut interdum mauris sem, non aliquet felis inter. dum sit --></p>
                 </div>
             </a>
         </div>
