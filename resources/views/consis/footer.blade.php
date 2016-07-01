b08b8fe45da4f32b3d0c4ddf577c69ab03e81bbe
vanfranrocha <vanfranrocha@hotmail.com>
2016-07-01 17:07:43 -0300

    caminho logo

--- resources/views/consis/footer.blade.php
+++ resources/views/consis/footer.blade.php
@@ -7,7 +7,7 @@
                 </div>
                 <!-- Menu -->
                <div class="row footer_top col-md-2">
-                <img style="width: 160px;height: 50px;" src="consis/images/header/logoconsis.png" alt="#">
+                <img style="width: 160px;height: 50px;" src="consis/images/header/LOGOCONSIS.png" alt="#">
             </div>
             </div>
         </div>
