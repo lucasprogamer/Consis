f290a5a64cd537b5dec5e389419c099da51a7108
lucasprogamer <lucasouzaweb@hotmail.com>
2016-07-06 23:35:09 -0300

    improve SEO

--- resources/views/layouts/consis.blade.php
+++ resources/views/layouts/consis.blade.php
@@ -8,28 +8,30 @@
     <title>I Consis</title>
      <!-- favicon -->
      <link rel="shortcut icon" sizes="16x16" type="image/png" href="consis/images/header/favicon.png"/>
+     <meta name="description" content="I CONSIS - Congresso de Sistemas de Informação FAHESA/ITPAC de 24 à 27 de outubro - Palestras, Minicursos, Submissão de Artigos, Campeonatos.">
+     <meta name="keywords" content="HTML,CSS,XML,JavaScript,Palestras,Jean carlos Suissa,normadev,minicursos,sistemas,wordpres,arduino,code dojo,league of legends, lol, cs, COUNTER STRIKE, artigos, submissão,tcc,evento">
     <!-- Bootstrap -->
     <meta name="google-site-verification" content="qym3HhFYBt4Y7Q7xcKifTswHIwYjDofyQO9CaQGeF5U" />
     <link href="{{ asset('consis/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('consis/css/bootstrap-theme.min.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('consis/css/bootstrap-theme.min.css') }}" rel="stylesheet">
     <!-- Font Awesome  -->
-    <link href="{{ asset('consis/css/font-awesome.min.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('consis/css/font-awesome.min.css') }}" rel="stylesheet">
     <!-- Countdown -->
-    <link href="{{ asset('consis/css/jquery.countdown.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('consis/css/jquery.countdown.css') }}" rel="stylesheet">
     <!-- Google Fonts -->
 
     <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
-    
+
     <!--    Owl Carousel css -->
-    <link rel="stylesheet" href="{{ asset('consis/css/owl.carousel.css') }}" type="text/css" media="screen"> 
-        
+    <link rel="stylesheet" href="{{ asset('consis/css/owl.carousel.css') }}" type="text/css" media="screen">
+
     <!-- LightBox -->
     <link rel="stylesheet" href="{{ asset('consis/vendors/Lightbox/lightbox.min.css') }}">
     <!-- Flexslider  -->
-    <link rel="stylesheet" href="{{ asset('consis/vendors/flexslider/flexslider.css') }}"> 
-    <link rel="stylesheet" href="{{ asset('consis/vendors/flexslider/preloader.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('consis/vendors/flexslider/flexslider.css') }}">
+    <link rel="stylesheet" href="{{ asset('consis/vendors/flexslider/preloader.css') }}">
     <!-- Gallery.scss -->
     <link rel="stylesheet" href="{{ asset('consis/scss/_gallery.scss') }}">
     <!-- style.css-->
@@ -46,77 +48,77 @@
   ga('send', 'pageview');
 
 </script>
-    
+
     <!--[if lt IE 9]>
       <script src="js/html5shiv.min.js"></script>
       <script src="js/respond.min.js"></script>
     <![endif]-->
 </head>
-<body> 
+<body>
     <div class="preloader">
       <div class="status">
       </div>
-      
+
     </div>
-    
+
     @include('consis.header')
 
-    
+
     @include('consis.summary')
-    
-    
+
+
     @include('consis.register')
 
     @include('consis.event')
 
     @include('consis.subscribe')
-    
-    
-    
+
+
+
 
     @include('consis.speakers')
    {{--@include('consis.pricing')--}}
 
-    
-    
+
+
 
 
     @include('consis.comments')
 
     @include('consis.courses')
 
-    
+
     <!-- Patrocinadores -->
     @include('consis.sponsors')
-    
+
     @include('consis.contact')
-    
+
     @include('consis.footer')
-    
-    
 
 
 
 
-   
+
+
+
     <!-- jQuery -->
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
     <script type="text/javascript" src="{{ asset('consis/vendors/flexslider/jquery.flexslider-min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('consis/js/jquery.countdown.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('consis/js/animatescroll/animatescroll.js') }}"></script>
     <script src="{{ asset('consis/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('consis/js/owl.carousel.min.js') }}"></script> 
-    
+    <script src="{{ asset('consis/js/owl.carousel.min.js') }}"></script>
+
     <!-- LightBox -->
     <script src="{{ asset('consis/vendors/Lightbox/lightbox.min.js') }}"></script>
     <!-- Mixitup JS -->
-    <script type="text/javascript" src="{{ asset('consis/vendors/mixitup/jquery.mixitup.min.js') }}"></script>  
+    <script type="text/javascript" src="{{ asset('consis/vendors/mixitup/jquery.mixitup.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('consis/js/preloader.js') }}"></script>
     <!-- Map JS -->
     <script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyAL3KSOmk3G8GaJvPDULfDCLoY73UFIAaU"></script>
     <script type="text/javascript" src="{{ asset('consis/vendors/map/gmaps.min.js') }}"></script>
     <script src="{{ asset('consis/vendors/map/map.js') }}"></script>
-     
-    <script src="{{ asset('consis/js/custome.js') }}"></script> 
+
+    <script src="{{ asset('consis/js/custome.js') }}"></script>
 </body>
 </html>
