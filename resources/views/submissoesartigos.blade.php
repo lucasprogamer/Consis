1bcb8a132791b7f3f1ddf7054d5fd4e27d6bf531
vanfranrocha <vanfranrocha@hotmail.com>
2016-08-03 15:55:38 -0300

    link submissoes

--- resources/views/submissoesartigos.blade.php
+++ resources/views/submissoesartigos.blade.php
@@ -70,7 +70,7 @@
 	            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">         
 	                <ul class="nav navbar-nav navbar-right">
 	                    <li><a href="/">Home</a></li>
-	                    <li><a href="#">Submeter</a></li>
+	                    <li><a href="/submissao">Submeter</a></li>
 	                </ul>
 	            </div>
 	        </nav>
