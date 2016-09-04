481720acd5ad93f8babe291b78815cf005b00896
lucasprogamer <lucasouzaweb@hotmail.com>
2016-09-04 18:43:32 +0100

    remove useless

--- resources/views/posts/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.adminex')
-
-@section('content')
-    @include('posts.show_fields')
-
-    <div class="form-group">
-           <a href="{!! route('posts.index') !!}" class="btn btn-default">Back</a>
-    </div>
-@endsection
