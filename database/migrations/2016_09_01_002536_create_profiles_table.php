97347ded24c6000e84ee41786d344e398d5d12e2
lucasprogamer <lucasouzaweb@hotmail.com>
2016-09-10 04:37:30 +0100

    rename class migration

--- database/migrations/2016_09_01_002536_create_profiles_table.php
+++ database/migrations/2016_09_01_002536_create_profiles_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateprofilesTable extends Migration
+class editprofilesTable extends Migration
 {
 
     /**
